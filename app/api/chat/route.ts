6c08523972cb206ba99398963b9e905077cb0ca4
Rajan Patel <rajan@stripe.com>
2025-03-25 00:44:09 +0000

    Sample AI Vid Clean

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -31,6 +31,21 @@ const stripeAgentToolkit = new StripeAgentToolkit({
   },
 });
 
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+// export async function POST(req: Request) {
+//   const { messages } = await req.json();
+
+//   const result = streamText({
+//     model: openai('gpt-4o'),
+//     messages,
+//   });
+
+//   return result.toDataStreamResponse();
+// }
+
 export async function POST(req: Request) {
   try {
     const { messages } = await req.json()
